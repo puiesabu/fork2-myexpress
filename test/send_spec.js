1285f19b4361828e1a317530ab08b78033b35056
kaeli <puiesabu@gmail.com>
2014-04-17 18:34:11 +0800

    Set content-length in res.send

--- test/send_spec.js
+++ test/send_spec.js
@@ -7,19 +7,22 @@ describe("res.send:", function() {
   var app;
   beforeEach(function() {
     app = express();
-    app.use("/buffer",function(req,res) {
-      res.send(new Buffer("binary data"));
-    });
-    app.use("/string",function(req,res) {
-      res.send("string data");
-    });
-    app.use("/json",function(req,res) {
-      res.type("json");
-      res.send("[1,2,3]");
-    });
   });
 
   describe("support buffer and string body:", function() {
+    beforeEach(function() {
+      app.use("/buffer",function(req,res) {
+        res.send(new Buffer("binary data"));
+      });
+      app.use("/string",function(req,res) {
+        res.send("string data");
+      });
+      app.use("/json",function(req,res) {
+        res.type("json");
+        res.send("[1,2,3]");
+      });
+    });
+
     it("responds to buffer", function(done) {
       request(app).get("/buffer")
         .expect(200)
@@ -36,6 +39,29 @@ describe("res.send:", function() {
       request(app).get("/json")
         .expect("[1,2,3]")
         .expect("Content-Type", "application/json").end(done);
+    });
+  });
+
+  describe("sets content-length:", function() {
+    beforeEach(function() {
+      app.use("/buffer",function(req,res) {
+        res.send(new Buffer("你好吗"));
+      });
+
+      app.use("/string",function(req,res) {
+        res.send("你好吗");
+      });
+    });
+
+    it("responds with the byte length of unicode string", function(done) {
+      request(app).get("/string")
+        .expect(200)
+        .expect("Content-Length", 9).end(done);
+    });
+    it("responds with the byte length of buffer", function(done) {
+      request(app).get("/buffer")
+        .expect(200)
+        .expect("Content-Length", 9).end(done);
     });
   });
 });
